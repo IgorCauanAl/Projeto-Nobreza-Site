b585b3a3107ec4fe62a572e478aa75dbc46e6f05
IgorCauanAl <igorcauawo@gmail.com>
2025-07-02 20:50:15 -0300

    Envio

--- src/Catalog/minicart.js
+++ src/Catalog/minicart.js
@@ -133,7 +133,7 @@ btnFinalizarCompra.addEventListener("click", () => {
     mostrarAlerta("Não é possível finalizar a compra sem itens no carrinho.");
   } else {
     // Redireciona para a página de checkout
-    window.location.href = "";
+    window.location.href = "../Car-market/index.html";
   }
 });
 
