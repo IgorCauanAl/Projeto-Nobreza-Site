d6c4bef667e1096321cb3fd4188a99638b7c5ff4
IgorCauanAl <igorcauawo@gmail.com>
2025-10-03 14:19:46 -0300

    Envio

---
+++ Back-End/nobreza-loja/nobreza-loja/src/main/java/commerce/nobreza/loja/masculina/nobreza_loja/SiteNobrezaLojaDeTernosMasculinaApplication.java
@@ -0,0 +1,13 @@
+package commerce.nobreza.loja.masculina.nobreza_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteNobrezaLojaDeTernosMasculinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteNobrezaLojaDeTernosMasculinaApplication.class, args);
+	}
+
+}
---
+++ Back-End/nobreza-loja/nobreza-loja/src/test/java/commerce/nobreza/loja/masculina/nobreza_loja/SiteNobrezaLojaDeTernosMasculinaApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.nobreza.loja.masculina.nobreza_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteNobrezaLojaDeTernosMasculinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
